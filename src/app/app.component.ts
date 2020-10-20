b9fe36f78e4552052d7383e4d6648221fb7e2302
Aleksandar <aca801@gmail.com>
2020-10-20 11:50:48 +0200

    Add empty product component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+  productItems = [
+    {
+      id: 1,
+      name: "Alcatel One",
+      price: 3333,
+      image: "http://placehold.it/250x250"
+    },
+    {
+      id: 2,
+      name: "Nokia 3310",
+      price: 440,
+      image: "http://placehold.it/250x250"
+    }
+  ];
 }
