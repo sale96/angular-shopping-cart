0c28baff6cc25f889008469b58210f13f750551c
Aleksandar <aca801@gmail.com>
2020-10-21 22:22:23 +0200

    Add navbar layout component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './components/store/product/product.component';
 import { ProductContainerComponent } from './components/store/product-container/product-container.component';
 import { CartComponent } from './components/store/cart/cart.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     ProductContainerComponent,
-    CartComponent
+    CartComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
