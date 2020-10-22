13e42651e89260a16cb42db5d6840c7c6e1ccd9e
Aleksandar <aca801@gmail.com>
2020-10-22 13:42:26 +0200

    Add logic for chaning quantity and delete if quantity is less then 1

--- src/app/components/store/cart-item/cart-item.component.ts
+++ src/app/components/store/cart-item/cart-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CartProductModel } from 'src/app/models/CartProductModel';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-item',
@@ -9,9 +10,17 @@ import { CartProductModel } from 'src/app/models/CartProductModel';
 export class CartItemComponent implements OnInit {
   @Input() item: CartProductModel;
 
-  constructor() { }
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+  }
+
+  decreseQuantity(event): void {
+    this.cartService.changeQuantity(this.item.productId, 'desc');
+  }
+
+  increseQuantity(event): void {
+    this.cartService.changeQuantity(this.item.productId, 'asc');
   }
 
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -27,4 +27,20 @@ export class CartService {
       this.cartItems.push(cartItem);
     }
   }
+
+  changeQuantity(id: number, status: string = "asc"): void {
+    if (status === 'asc') {
+      this.cartItems.find(x => x.productId === id).quantity++;
+    } else {
+      if (this.cartItems.find(x => x.productId === id).quantity != 1) {
+        this.cartItems.find(x => x.productId === id).quantity--;
+      } else {
+        this.deleteCartItem(id);
+      }
+    }
+  }
+
+  deleteCartItem(id: number) {
+    this.cartItems = this.cartItems.filter(x => x.productId !== id);
+  }
 }
